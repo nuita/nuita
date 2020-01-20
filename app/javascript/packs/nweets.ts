14b20ec06c5ef4435d4fe3d828c277696064a4ae
kypkyp <kyp@kmc.gr.jp>
2020-01-21 00:22:33 +0900

    無名関数使うのやめて多重起動抑制&呼び出し元をpagination.tsに統一

--- app/javascript/packs/nweets.ts
+++ app/javascript/packs/nweets.ts
@@ -1,28 +1,28 @@
+let setIndividualLikeButton = (event:Event) => {
+  let div = <HTMLElement>event.currentTarget;
+
+  let anchor = <HTMLElement>div.firstElementChild;
+  let icon = <HTMLElement>anchor.childNodes[0];
+  let likeFlash = <HTMLElement>anchor.childNodes[1];
+  let outerLatestLikedTime = <HTMLElement>anchor.childNodes[2];
+  // let likeNumElement = <HTMLElement>anchor.childNodes[1];
+  anchor.classList.toggle('liked');
+
+  if(icon.classList.contains('fas')){
+    icon.classList.replace('fas', 'far');
+    anchor.setAttribute('data-method', 'post');
+    outerLatestLikedTime.classList.remove('d-none');
+    likeFlash.innerText = '';
+  }else{
+    icon.classList.replace('far', 'fas');
+    anchor.setAttribute('data-method', 'delete');
+    likeFlash.innerText = "「いいね」しました！";
+    outerLatestLikedTime.classList.add('d-none');
+  }
+}
+
 export function setLikeButtons(){
   document.querySelectorAll('.like-btn').forEach(function(div){
-    div.addEventListener('ajax:success', function(){
-      var anchor = <HTMLElement>div.firstElementChild;
-      var icon = <HTMLElement>anchor.childNodes[0];
-      var likeFlash = <HTMLElement>anchor.childNodes[1];
-      var outerLatestLikedTime = <HTMLElement>anchor.childNodes[2];
-      // var likeNumElement = <HTMLElement>anchor.childNodes[1];
-      anchor.classList.toggle('liked');
-
-      if(icon.classList.contains('fas')){
-        icon.classList.replace('fas', 'far');
-        anchor.setAttribute('data-method', 'post');
-        outerLatestLikedTime.classList.remove('d-none');
-        likeFlash.innerText = '';
-      }else{
-        icon.classList.replace('far', 'fas');
-        anchor.setAttribute('data-method', 'delete');
-        likeFlash.innerText = "「いいね」しました！";
-        outerLatestLikedTime.classList.add('d-none');
-      }
-    })
+    div.addEventListener('ajax:success', setIndividualLikeButton);
   });
 };
-
-document.addEventListener('turbolinks:load', function(){
-  setLikeButtons();
-});
