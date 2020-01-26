1b12e734133560f4ef710903ac52c5d76f82c2a7
kypkyp <kyp@kmc.gr.jp>
2020-01-26 15:43:34 +0900

    いい感じのJSによるいい感じのフォーム

--- app/javascript/packs/categories.ts
+++ app/javascript/packs/categories.ts
@@ -1,3 +1,4 @@
+// 既に投稿されたオカズにタグつけるやつ (category#createとかに送る)
 let setIndividualCategoryButton = (event:Event) => {
   let a = <HTMLElement>event.currentTarget;
 
@@ -15,8 +16,33 @@ let setIndividualCategoryButton = (event:Event) => {
   }
 }
 
+// 新しく投稿するときにタグつけるやつ (JSでhidden_fieldの値変える)
+let setIndividualToggleTagButton = (event: Event) => {
+  let a = <HTMLElement>event.currentTarget;
+  let field = <HTMLElement>a.lastChild;
+
+  let n = parseInt(field.getAttribute('value'));
+  field.setAttribute('value', (1 - n).toString());
+  console.log(1 - n);
+
+  let badge = <HTMLElement>a.firstChild;
+  let i = <HTMLElement>badge.childNodes[2].firstChild;
+
+  if(i.classList.contains('fa-plus')){
+    i.classList.replace('fa-plus', 'fa-times');
+  }else{
+    i.classList.replace('fa-times', 'fa-plus');
+  }
+
+  badge.classList.toggle('active');
+};
+
 export function setCategoryButtons(){
-  document.querySelectorAll('.tag-link').forEach(function(div){
+  document.querySelectorAll('.nweet-tag-link').forEach(function(div){
     div.addEventListener('ajax:success', setIndividualCategoryButton);
+  });
+
+  document.querySelectorAll('.toggle-tag-link').forEach(function(div){
+    div.addEventListener('click', setIndividualToggleTagButton);
   });
 };
