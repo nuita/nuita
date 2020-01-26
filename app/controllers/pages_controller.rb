8f652c9f3ef94e532ae272aac14abb31378fe6f2
kypkyp <kyp@kmc.gr.jp>
2020-01-26 15:54:16 +0900

    nweet#new新設、#nuita押すとここに遷移

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,7 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
-      if params[:success] == 'true'
-        @nweet = Nweet.find_by(url_digest: params[:url_digest])
-      else
-        @nweet = current_user.nweets.build
-      end
+      @nweet = current_user.nweets.build
       @feed_items = current_user.timeline.paginate(page: params[:page])
       @timeline = true
 
