7ab17b8a3aaf64a716a480c212fdda545d4bb763
kyp <kyp@Adam.local>
2020-04-09 21:42:21 +0900

     credentials読み取れなかったときにエラー返さないようにnilチェックっぽいのする

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret]
+if Rails.env.production?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret]
+  end
+else
+  OmniAuth.config.test_mode = true;
 end
