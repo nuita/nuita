f06b718343c2ea9d534b377233b681f6c216d5cf
kyp <kyp@kmc.gr.jp>
2020-04-26 00:08:46 +0900

    navbarで表示中の画面のアイコンは光るようにする

--- app/frontend/src/header.ts
+++ app/frontend/src/header.ts
@@ -1,13 +1,30 @@
 // ロゴを押すとトップへ戻る
-export default function setHeaderButton(){
+export default function setHeaderButton() {
   let topButton = document.getElementById('navbarBrandIcon');
 
-  if(topButton){
-    topButton.addEventListener('click', ():void => {
+  if (topButton) {
+    topButton.addEventListener('click', (): void => {
       document.body.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
       });
     })
+  }
+
+  let pathname: string = location.pathname;
+  let homeNav = document.getElementById("home-nav");
+  let notificationsNav = document.getElementById("notifications-nav");
+
+  if (homeNav && notificationsNav) {
+    if (pathname == "/") {
+      homeNav.classList.add("active");
+      notificationsNav.classList.remove("active");
+    } else if (pathname == "/notifications") {
+      homeNav.classList.remove("active");
+      notificationsNav.classList.add("active");
+    } else {
+      homeNav.classList.remove("active");
+      notificationsNav.classList.remove("active");
+    }
   }
 }
