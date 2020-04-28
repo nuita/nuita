763a04379d43393148ba4a9c4200e4be046222bc
kyp <kyp@kmc.gr.jp>
2020-04-29 00:17:08 +0900

    C, Vでも使ってみる

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 class LinksController < ApplicationController
   def recommend
     category_editable = true if user_signed_in?
-    render partial: 'cards/horizontal', locals: {link: Link.recommend, genre_hidden?: !category_editable}
+    
+    render partial: 'cards/horizontal', locals: {link: Link.recommend(displayable: params[:displayable]), genre_hidden?: !category_editable}
   end
 end
