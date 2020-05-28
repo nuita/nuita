35852e60a7e34633373eb8e0417c0e47315446de
ahis1503 <ahis1503@gmail.com>
2020-05-28 11:46:39 +0000

    タイムラインのページング処理を削除

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @nweet = current_user.nweets.build
-      @feed_items = current_user.timeline.paginate(page: params[:page])
+      @feed_items = current_user.timeline.limit(10)
       @timeline = true
 
       if params[:scroll]
