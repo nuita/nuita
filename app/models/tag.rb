05debd7b5c526d279634db77687b59192c7f2a0a
kyp <kyp@kmc.gr.jp>
2020-07-05 19:04:26 +0900

    dependent: destroy忘れてた

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,8 @@ class Tag < ApplicationRecord
   has_many :link_tags, dependent: :destroy
   has_many :links, through: :link_tags
 
+  has_many :preferences, dependent: :destroy
+
   before_save :set_censored_by_default
   before_validation { name.upcase! }
 
