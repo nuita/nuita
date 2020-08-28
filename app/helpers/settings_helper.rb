1d6a316fd447ef534f166b8094eae1e2cfdd88f5
kyp <kyp@kmc.gr.jp>
2020-08-28 22:49:50 +0900

    検閲設定のパーシャルを抽象化・共通化

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -2,4 +2,12 @@ module SettingsHelper
   def feed_scope_options
     User.feed_scopes.map { |k, v| [User.human_attribute_name("user.feed_scopes.#{k}"), k] }
   end
+
+  def taglist_id(context)
+    "tagsFormTagList#{context}"
+  end
+
+  def tags_form_path(context, tag = nil)
+    "/#{context}" + (tag ? "?tag=#{tag}" : "")
+  end
 end
