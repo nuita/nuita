a37c21927c6a25ce95d10b626254df994e71870b
kyp <kyp@kmc.gr.jp>
2020-09-13 11:57:45 +0900

    fix #244

--- config/environment.rb
+++ config/environment.rb
@@ -5,18 +5,20 @@ require_relative 'application'
 Rails.application.initialize!
 
 Rails.application.configure do
-  # Don't care if the mailer can't send.
+
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: Rails.application.credentials.ses[:address],
-    port: 587,
-    domain: Rails.application.credentials.ses[:domain],
-    authentication: :login,
-    user_name: Rails.application.credentials.ses[:access_key],
-    password: Rails.application.credentials.ses[:secret_key],
-    enable_starttls_auto: true 
-  }
+  if Rails.application.credentials.ses
+    config.action_mailer.smtp_settings = {
+      address: Rails.application.credentials&.ses[:address],
+      port: 587,
+      domain: Rails.application.credentials.ses[:domain],
+      authentication: :login,
+      user_name: Rails.application.credentials.ses[:access_key],
+      password: Rails.application.credentials.ses[:secret_key],
+      enable_starttls_auto: true
+    }
+  end
 end
