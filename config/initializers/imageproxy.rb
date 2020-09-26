abf49ecac1b5783b0e29b40fb0bbb5cfca42ef7a
kyp <kyp@kmc.gr.jp>
2020-09-26 12:38:21 +0900

    nil超安全

--- config/initializers/imageproxy.rb
+++ config/initializers/imageproxy.rb
@@ -1,2 +1,2 @@
-Rails.application.config.x.imageproxy.host = ENV["IMAGEPROXY_HOST"] || Rails.application.credentials.imageproxy.values_at(:host)
-Rails.application.config.x.imageproxy.secret_key = ENV["IMAGEPROXY_KEY"] || Rails.application.credentials.imageproxy.values_at(:secret_key)
+Rails.application.config.x.imageproxy.host = ENV["IMAGEPROXY_HOST"] || Rails.application.credentials.imageproxy&.values_at(:host)
+Rails.application.config.x.imageproxy.secret_key = ENV["IMAGEPROXY_KEY"] || Rails.application.credentials.imageproxy&.values_at(:secret_key)
