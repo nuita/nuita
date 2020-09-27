c29919bddd579ee5e5fa5a3c67f74a882b05d87d
kyp <kyp@kmc.gr.jp>
2020-09-27 12:09:30 +0900

    通知一覧のN+1修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @notifications = current_user.passive_notifications.paginate(page: params[:page], per_page: 100)
+    @notifications = current_user.passive_notifications.includes(:origin).paginate(page: params[:page], per_page: 100)
     current_user.check_notifications
   end
 
