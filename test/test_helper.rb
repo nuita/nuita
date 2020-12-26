6a0c3b67859a19dbe18934f9f9dbba08aa8d140e
kyp <kyp@kmc.gr.jp>
2020-12-27 00:57:37 +0900

    codecovにアップロード(公式通り素直にRuby側から上げる)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+SimpleCov.start 'rails'
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
