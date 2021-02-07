b146b0eecd644e8421cdbb1fc9e2dc5bf73bd121
kyp <kyp@kmc.gr.jp>
2021-02-07 18:44:13 +0900

    コールバックの名前を変更

--- app/models/nweet.rb
+++ app/models/nweet.rb
@@ -15,8 +15,8 @@ class Nweet < ApplicationRecord
   validates :user_id, presence: true
   validates :did_at, presence: true
   validates :statement, length: {maximum: 100}
-  validate :did_at_past?
-  validate :have_enough_interval?, on: :create
+  validate :past?
+  validate :has_enough_interval?, on: :create
 
   default_scope -> { order(did_at: :desc) }
 
@@ -24,14 +24,13 @@ class Nweet < ApplicationRecord
 
   self.per_page = 10
 
-  def did_at_past?
+  def past?
     if did_at # did at is not nil
       errors.add(:did_at, ' is not in the past') unless did_at <= Time.zone.now
     end
   end
 
-  # may need refactoring
-  def have_enough_interval?
+  def has_enough_interval?
     if user.nweets.count != 0 && did_at && did_at < user.nweets.first.did_at + 3.minutes
       errors.add(:did_at, ' has not enough interval')
     end
