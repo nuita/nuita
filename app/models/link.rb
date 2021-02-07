72b2717c66cba52758e71df735261bdf23495b16
kyp <kyp@kmc.gr.jp>
2021-02-08 00:39:14 +0900

    Linkにlegal?とfeaturable?を追加

--- app/models/link.rb
+++ app/models/link.rb
@@ -14,6 +14,9 @@ class Link < ApplicationRecord
 
   scope :displayable, -> { where.not(image: [nil, '']).left_outer_joins(:tags).where(tags: {censored_by_default: nil}) }
 
+  # Resolvers where nweets might contain illegal contents.
+  UNSAFE_RESOLVERS = ['', 'Image'].freeze
+
   # Should be used only in link_task:refetch_all
   def refetch
     panchira = Panchira.fetch(url)
@@ -49,6 +52,15 @@ class Link < ApplicationRecord
     link_tags.destroy_all
   end
 
+  def legal?
+    r = resolver.match(/Panchira::([a-zA-Z]*)Resolver/).to_a[1]
+    UNSAFE_RESOLVERS.exclude?(r)
+  end
+
+  def featurable?
+    legal? && image.present?
+  end
+
   class << self
     def recommend(displayable: false)
       link = displayable ? Link.displayable : Link.all
@@ -63,7 +75,7 @@ class Link < ApplicationRecord
       link = Link.find_or_initialize_by(url: canonical_url)
 
       link.update(hash_panchira(panchira))
-      link.set_tags(panchira.tags, destroy_existing_tags: false)
+      link.set_tags(panchira.tags, destroy_existing_tags: false) if panchira.tags
 
       link
     end
@@ -78,7 +90,8 @@ class Link < ApplicationRecord
         image: panchira.image.url,
         image_width: panchira.image.width,
         image_height: panchira.image.height,
-        url: panchira.canonical_url
+        url: panchira.canonical_url,
+        resolver: panchira.resolver
       }
     end
   end
