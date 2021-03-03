b092e446d4656e43d79fb74cbcedeb362b51112f
kyp <kyp@kmc.gr.jp>
2021-03-03 22:49:35 +0900

    ロゴの色も分ける

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,8 @@ module PagesHelper
   def navbar_type(landing)
     landing.present? ? 'landing-navbar' : 'guest-navbar'
   end
+
+  def navbar_logo(landing)
+    landing.present? ? 'logo_white.svg' : 'logo_color.svg'
+  end
 end
