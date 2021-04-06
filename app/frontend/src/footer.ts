0a1cace600b877ecedccb5f628f6b6dcbeb14038
kyp <kyp@kmc.gr.jp>
2021-04-07 01:06:36 +0900

    ヘッダーもslimからactiveなnavを判定する

--- app/frontend/src/footer.ts
+++ app/frontend/src/footer.ts
@@ -1,16 +1,10 @@
 export function setFooterButtons() {
   const activeModeDetector = document.getElementById('activeModeDetector');
-  if (activeModeDetector === null) {
-    return;
-  }
-
-  const pageNavMode = activeModeDetector.getAttribute('data-nav-mode');
+  const pageNavMode = activeModeDetector?.getAttribute('data-nav-mode');
   if (pageNavMode === null) {
     return;
   }
 
   const icon = document.getElementById(`userFooterListItem${pageNavMode}`);
-  if (icon !== null) {
-    icon.classList.add('active');
-  }
+  icon?.classList?.add('active');
 }
