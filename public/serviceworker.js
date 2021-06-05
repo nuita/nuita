a43c7f6d0508945a6fdb71b53701922d41f22451
kyp <kyp@kmc.gr.jp>
2021-06-05 15:11:25 +0900

    雑キャッシュ

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -5,5 +5,17 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
+  if (!event.request.url.match(/assets|packs/)) {
+    return;
+  }
+
+  caches.match(event.request).then((res) => {
+    return res || fetch(event.request).then((response) => {
+      return caches.open("v1").then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  });
 });
 
