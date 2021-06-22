4c9f38e95762726e3edbe1c216b719a014a7519c
kyp <kyp@kmc.gr.jp>
2021-06-22 22:42:43 +0900

    JS経由でdatetime_fieldの日付を挿入/削除する

--- app/frontend/src/new_nweet_form.ts
+++ app/frontend/src/new_nweet_form.ts
@@ -26,9 +26,19 @@ const setDatetimeForm = () => {
   }
 
   const form = document.getElementById('newNweetDatetimeFormWrapper');
+  const dateInput = <HTMLFormElement>document.getElementById('newNweetDatetimeFormInput');
 
   prompt.addEventListener('click', (e) => {
     e.preventDefault();
+
+    const now = convertDateToDOMFormat(new Date());
+
+    // ugly but effective https://stackoverflow.com/a/31665235
+    const minDate = new Date(new Date().setDate(new Date().getDate() - 30));
+
+    dateInput.value = now;
+    dateInput.max = now;
+    dateInput.min = convertDateToDOMFormat(minDate);
 
     form.classList.remove('d-none');
     prompt.classList.add('d-none');
@@ -38,7 +48,15 @@ const setDatetimeForm = () => {
   closeButton.addEventListener('click', (e) => {
     e.preventDefault();
 
+    dateInput.value = null;
+    dateInput.max = null;
+    dateInput.min = null;
     prompt.classList.remove('d-none');
     form.classList.add('d-none');
   });
 };
+
+const convertDateToDOMFormat = (date: Date) => {
+  date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  return date.toISOString().slice(0, 16);
+}
